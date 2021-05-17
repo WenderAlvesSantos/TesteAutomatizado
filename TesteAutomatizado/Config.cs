0e2fa68532541561054d6aa615d9f7b0c48a0c1f
Wender Alves dos Santos <x000418@emgea.ativos>
2021-05-17 14:21:08 -0300

    Add project files.

---
+++ TesteAutomatizado/Config.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System.Threading;
+
+namespace TesteAutomatizado
+{
+    public class Config
+    {
+        public class Metodos
+        {
+            ChromeDriver driver = new ChromeDriver();
+
+            public string screenshotsPasta = @"C:\Users\x000418\Pictures\Print Teste Automatizados\";
+
+            public void Maximizar()
+            {
+                driver.Manage().Window.Maximize();
+            }
+
+            public void Escrever(string XPath, string texto)
+            {
+                driver.FindElement(By.XPath(XPath)).SendKeys(texto);
+            }
+
+            public void Navegar(string URL)
+            {
+                driver.Navigate().GoToUrl(URL);
+            }
+
+            public void Enter()
+            {
+                driver.FindElement(By.Name("q")).SendKeys(Keys.Enter);
+            }
+
+            public void Click(string XPath)
+            {
+                driver.FindElement(By.XPath(XPath)).Click();
+            }
+
+            public void Close()
+            {
+                driver.Close();
+            }
+
+            public void Esperar(int tempo)
+            {
+                Thread.Sleep(tempo);
+            }
+
+            public void Screenshot(string nomeArquivo)
+            {
+                Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
+                ss.SaveAsFile(screenshotsPasta + nomeArquivo + ".png",
+                ScreenshotImageFormat.Png);
+            }
+        }
+
+    }
+}
---
+++ TesteAutomatizado/UnitTest1.cs
@@ -0,0 +1,35 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using static TesteAutomatizado.Config;
+
+namespace TesteAutomatizado
+{
+    [TestClass]
+    public class Tests
+    {
+        [Test]
+        public static void AmbienteTeste()
+        {
+            Metodos metodos = new Metodos();
+
+            metodos.Maximizar();
+
+            metodos.Navegar("http://google.com");
+
+            metodos.Escrever("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input", "data atual");
+
+            metodos.Enter();
+
+            metodos.Click("//*[@id='gb']/div/div[1]/a");
+
+            metodos.Esperar(2000);
+
+            metodos.Screenshot("Mais um teste");
+
+            metodos.Close();
+        }
+    }
+}
